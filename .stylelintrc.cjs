9194033f6d9356817cd8d0639350bd159ece5a2d
霍铭浩 <huominghao@crland.com.cn>
2022-09-08 16:40:07 +0800

    chore: add stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: [],
+  rules: {
+    'string-quotes': 'single', // 单引号
+    'function-url-quotes': 'always', // url使用引号
+    'declaration-empty-line-before': null, // 声明前允许有空行
+    'alpha-value-notation': 'number', // 用数字表示alpha的值
+    'color-function-notation': 'legacy', // 颜色函数使用旧版表示法
+  },
+  ignoreFiles: [
+    '/dist/*',
+    'public/*',
+    '*.js',
+    '*.ts',
+    '*.png',
+    '*.jpg',
+    '*.webp',
+    '*.ttf',
+    '*.woff',
+  ],
+};
