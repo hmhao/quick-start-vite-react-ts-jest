d93d5498190f1cd97f1a576868e4d4694c4a7d88
霍铭浩 <huominghao@crland.com.cn>
2022-09-08 13:55:21 +0800

    chore: add postcss plugins and resolve alias

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // 现代 css 语法
+    'postcss-preset-env': {},
+    // 类Sass嵌套
+    'postcss-nested': {},
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteEslint from 'vite-plugin-eslint';
+import path from 'path';
+
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    // 配置项目别名
+    alias: {
+      '@': _resolve('src'),
+    },
+  },
   plugins: [
     react(),
     viteEslint({
